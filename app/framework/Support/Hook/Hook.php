0321b50158fee150851ad02dbc58687484010de6
Guillaume <guillaume.turpin45@gmail.com>
2023-06-05 17:12:54 +0200

    :sparkles: Adding Hook abstract class

---
+++ app/framework/Support/Hook/Hook.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace OOWPrise\Support\Hook;
+
+use OOWPrise\Support\Facades\Action;
+use OOWPrise\Support\Facades\Filter;
+use OOWPrise\Support\Facades\Contracts\HookInterface;
+
+/**
+ * @see HookInterface
+ */
+abstract class Hook {
+
+	/**
+	 * The hook name.
+	 *
+	 * @var string
+	 */
+	protected string $hook;
+
+	/**
+	 * The hook type.
+	 * Can be either "action" or "filter".
+	 *
+	 * @var string
+	 */
+	protected string $hook_type = 'action';
+
+	/**
+	 * The priority of the hook.
+	 *
+	 * @var integer
+	 */
+	protected int $priority = 10;
+
+	/**
+	 * The number of arguments accepted by the callback function.
+	 *
+	 * @var integer
+	 */
+	protected int $accepted_args = 1;
+
+	/**
+	 * Register the hook with the "callback" method which must be defined in the child class.
+	 *
+	 * @return void
+	 */
+	public function register(): void {
+		$this->check_callback_method_exists();
+
+		if ( $this->hook_type === 'filter' ) {
+			Filter::add( $this->hook, [ $this, 'callback' ], $this->priority, $this->accepted_args );
+		}
+
+		Action::add( $this->hook, [ $this, 'callback' ], $this->priority, $this->accepted_args );
+	}
+
+	/**
+	 * Check if the callback method exists.
+	 * The callback method must be implemented by the child class.
+	 * But it can have any parameters number or none, so we cannot create it in Interface or Abstract method.
+	 *
+	 * @return boolean
+	 */
+	private function check_callback_method_exists(): bool {
+		if ( ! method_exists( $this, 'callback' ) ) {
+			throw new \Exception( 'The "callback" method must be implemented by the child class.' );
+		}
+
+		return true;
+	}
+}
